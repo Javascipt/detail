a306c06948b026eb5e52b9886061eb9ca75dec88
Khalid REHIOUI <Rehioui@gmail.com>
2015-09-26 19:58:25 +0100

    Using ejs as template engine

--- modules/generator.js
+++ modules/generator.js
@@ -1,4 +1,5 @@
 var marked = require('marked');
+var ejs = require("ejs");
 
 module.exports = (function (marked) {
   function generateReadme (markdown) {
@@ -8,75 +9,42 @@ module.exports = (function (marked) {
     if(data.author && data.author.name) {
       data.author = data.author.name;
     }
-    var dependenciesCallback = function (item, index, array) { 
-      return '<a href="http://npmjs.org/package/' + item + '" target="_blank">'
-           + '<span class="btn label label-success">' + item + ' ' + data.dependencies[item] + '</span></a>';
+    return {
+      'name': data.name,
+      'global': data.preferGlobal,
+      'description': data.description || '',
+      'author': (data.author.replace( /\<(.*?)\>/g, '').replace( /\[(.*?)\]/g, '') || ''),
+      'email': ((data.author.match( /\<(.*?)\>/g, '') || [''])[0]).replace('<','').replace('>',''),
+      'website': ((data.author.match( /\((.*?)\)/g, '') || [''])[0]).replace('(','').replace(')',''),
+      'repo': (data.repository && data.repository.url && data.repository.url.split('+').pop() || ''),
+      'dependencies': (data.dependencies || {}),
+      'tags': (data.keywords || []),
+      'version': data.version,
+      'license': (data.license || '')
     };
-    // @todo : use a template engine instead
-	var html  = '<div class="panel panel-default">'
-              + '  <!-- Default panel contents -->'
-              + '  <div class="panel-heading">Project Details</div>'
-              + '  <div class="panel-body">'
-              + '    <table class="table table-stripped table-bordered">'
-              + '      <tr>'
-              + '         <td> Package name </td>'
-              + '         <td> '+ data.name + ( data.preferGlobal ? '  <span class="btn label label-default"> Global </span>':'') + '</td>'
-              + '      </tr>'
-              + '      <tr> <td> Description </td><td> ' + (data.description || '') + ' </td> </tr>'
-              + '      <tr> <td> Author </td> <td>' + (data.author.replace( /\<(.*?)\>/g, '').replace( /\[(.*?)\]/g, '') || '') + '</td> </tr>'
-              + '      <tr> <td> Email </td> <td>' + ((data.author.match( /\<(.*?)\>/g, '') || [''])[0]).replace('<','').replace('>','') + '</td> </tr>'
-              + '      <tr> <td> Website </td> <td> ' + ((data.author.match( /\[(.*?)\]/g, '') || [''])[0]) + '</td> </tr>'
-              + '      <tr> <td> Repository </td><td>' + (data.repository && data.repository.url && data.repository.url.split('+').pop() || '') + '</td></tr>'
-              + '    </table>'
-              + '  </div>'
-              + '  <!-- List group -->'
-              + '  <ul class="list-group">'
-              + '    <li class="list-group-item">'
-              + '      <b>Dependencies : </b>' + (data.dependencies ? Object.keys(data.dependencies).map(dependenciesCallback).join(' ') : '')
-              + '    </li>'
-              + '    <li class="list-group-item">'
-              + '      <b>Tags : </b>' + (data.keywords || []).map(function(tag) { return '<span class="label label-info">' + tag + '</span>' }).join(' ')
-              + '    </li>'
-              + '    <li class="list-group-item">'
-              + '      <b>Version : </b> <span class="label label-primary">' + data.version + '</span>'
-              + '    </li>'
-              + '    <li class="list-group-item">'
-              + '      <b>Liscence : </b>'
-              + '      <span class="label label-warning">' + (data.license || '') + '</span>'
-              + '    </li>'
-              + '  </ul>'
-              + '</div>';
-    
-    return html;
   }
-  function generateBugReporter (data) {
-    return (data.bugs && data.bugs.url) ? ('<li><a class="menu-btn" target="_blank" href="' + data.bugs.url + '">Report bug</a></li>') : '';
-  }
-  function generateMainPage (html, callback) {
+  function generateMainPage (data, callback) {
     var path = require("path");
-    var pathToTemplate = path.resolve(__dirname, '..', 'templates', require("../config.json").template, 'template.html');
+    var pathToTemplate = path.resolve(__dirname, '..', 'templates', require("../config.json").template, 'template.ejs');
     var pathToMainPage = path.resolve(__dirname, '..', 'templates', require("../config.json").template, 'index.html');
     var fs = require('fs');
     fs.readFile(pathToTemplate, function (err, template) {
       if(err) return callback(err);
-      var htmlCode = template.toString()
-          .replace("{{ @readme }}", html["README.md"])
-          .replace("{{ @details }}", html["details"])
-          .replace("{{ @report }}", html["bugReport"]);
-      fs.writeFile(pathToMainPage, htmlCode, callback);
+      fs.writeFile(pathToMainPage, ejs.render(template.toString(), data), callback);
     });
   }
   return {
     generate: function (filesPaths, callback) {
       var path = require("path");
       var fs = require('fs');
-      var html = {};
+      var data = {};
       fs.readFile(filesPaths['README.md'], function (err, markdown) {
         if(err) return callback(err);
-        html['README.md'] = generateReadme(markdown.toString());
-        html['details'] = generateDetails(require(path.resolve(filesPaths['package.json'])));
-        html['bugReport'] = generateBugReporter(require(path.resolve(filesPaths['package.json'])));
-        generateMainPage(html, callback);
+        var pkgJson = require(path.resolve(filesPaths['package.json']));
+        data = generateDetails(pkgJson);
+        data.readme = generateReadme(markdown.toString());
+        data.bug = pkgJson.bugs && pkgJson.bugs.url;
+        generateMainPage(data, callback);
       });
     }
   }
