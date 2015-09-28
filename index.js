874001e7f3b7edb5d87e088b5aac66da0e4368f7
Khalid REHIOUI <Rehioui@gmail.com>
2015-09-28 15:05:46 +0100

    Clarify usage info + typo

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var generator = require("./modules/generator");
 var launcher = require("./modules/launcher");
 
 cli.when('-h --help', 'Output usage information', cli.help)
-   .when('-v --version', 'OUtput version number', cli.version)
+   .when('-v --version', 'Output version number', cli.version)
    .when('-m --module <moduleName>', 'Get details of locally installed modules', function (pkgName) {
       if(arguments.length != 1) return cli.throw("You need to specify the name of only one global package");
       getFilesPaths(getPath(process.cwd(), pkgName), function (err, pathsToFiles) {
@@ -32,7 +32,7 @@ cli.when('-h --help', 'Output usage information', cli.help)
           })
         }.bind(cli));
       }.bind(cli));
-   }).default('detail [dir] [moduleName]', 'Launch details and markdown viewer of given project/package', function (dir, pkgName) {
+   }).default('detail [options|dir] [moduleName]', 'Launch details and markdown viewer of given project/package', function (dir, pkgName) {
       dir = dir || process.cwd();
       getFilesPaths(getPath(dir, pkgName), function (err, pathsToFiles) {
         if(err) return this.throw(err.message);
